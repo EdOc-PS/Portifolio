04df32945ae67ef5d5f686d08cb452d6e0412ff7
Eduardo Octávio <94386792+EdOc-PS@users.noreply.github.com>
2023-12-20 21:20:07 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,24 @@
 const variable = {
-    menu: document.querySelector('.menu'),
-    menuExpnd: document.querySelector('#menu-expand'),
+    nav: {
+        menu: document.querySelector('.menu'),
+        btnMenuExpand: document.querySelector('#btn-menu-expand'),
+        btnMenu: document.querySelectorAll('.btn-menu'),
+        btnmenuActive: document.querySelector('.btn-active'),
+        txtMenu: document.querySelectorAll('.menu-txt'),
+        txtMenuActive: document.querySelector('actived'),
 
+
+    }
 };
 
 
 //menu se expandir
-variable.menuExpnd.addEventListener('click', function(){
-    variable.menu.classList.toggle("expanded");
+variable.nav.btnMenuExpand.addEventListener('click', function () {
+    variable.nav.menu.classList.toggle("expanded");
+    variable.nav.btnMenuExpand.forEach((variable) =>{
+        variable.nav.btnMenu.classList.toggle("btn-active");
+    });
+    variable.nav.txtMenu.forEach((variable) => {
+        variable.nav.txtMenu.classList.toggle("actived");
+    });
 })
