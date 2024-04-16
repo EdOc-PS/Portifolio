07d4f9a5f077dfafffffae057a21ef603f217003
Eduardo Octávio <eeuardooctavio@gmail.com>
2024-04-16 00:39:58 -0300

    u-0.5

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,17 @@
 const variable = {
     nav: {
         menu: document.querySelector('.menu'),
-        btnMenuExpand: document.querySelector('#menu-expand-btn'),
+        btnMenuExpand: document.getElementById('menu-expand-btn'),
         btnMenu: document.querySelectorAll('.menu-a-btn'),
         txtMenu: document.querySelectorAll('.menu-txt'),
+        btnColor: document.getElementById('btn-color')
     },
+    icons:{
+        iconBtnMenu: document.querySelector('.fi.fi-sr-menu-burger'),
+        iconBtnColor: document.querySelector('.fi.fi-sr-circle'),
+    }
+    
+    ,
     section: document.querySelectorAll('.hidden')
 };
 
@@ -14,7 +21,14 @@ const variable = {
 //menu se expandir
 variable.nav.btnMenuExpand.addEventListener('click', function () {
     variable.nav.menu.classList.toggle("expanded");
+    variable.icons.iconBtnMenu.classList.toggle("fi-sr-x");
 })
+//btn de light mode
+variable.nav.btnColor.addEventListener('click', function () {
+    variable.icons.iconBtnColor.classList.toggle("fi-br-sun");
+    variable.nav.menu.classList.toggle("light")
+})
+
 
 //botão fazendo menu se desexpandir
 variable.nav.btnMenu.forEach(function (btn) {
