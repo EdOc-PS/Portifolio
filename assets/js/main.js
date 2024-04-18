476c581a1b5621686a68ffb62668d0086bc3faca
Eduardo Octávio <eeuardooctavio@gmail.com>
2024-04-17 23:28:33 -0300

    U-0.51

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,25 +8,51 @@ const variable = {
     },
     icons:{
         iconBtnMenu: document.querySelector('.fi.fi-sr-menu-burger'),
-        iconBtnColor: document.querySelector('.fi.fi-sr-circle'),
-    }
-    
-    ,
+        iconBtnColor: document.querySelector('.fi.fi-sr-brightness'),
+        iconBtnTxt: document.querySelectorAll('.menu-icon i'),
+        iconBtnExplorer: document.querySelector('.home-a-btn i'),
+    },
     section: document.querySelectorAll('.hidden')
 };
 
+var toLight = {
+    body: document.querySelector("body"),
+    socialContainer: document.querySelector('.about-social-container'),
+    btnRepository: document.querySelector('.discover-repository'),
+    homeh1: document.getElementById('home-h1'),
+    btnHome: document.querySelector('.home-a-btn'),
 
-
+}
 
 //menu se expandir
 variable.nav.btnMenuExpand.addEventListener('click', function () {
     variable.nav.menu.classList.toggle("expanded");
     variable.icons.iconBtnMenu.classList.toggle("fi-sr-x");
 })
+
+
 //btn de light mode
 variable.nav.btnColor.addEventListener('click', function () {
-    variable.icons.iconBtnColor.classList.toggle("fi-br-sun");
-    variable.nav.menu.classList.toggle("light")
+
+        variable.icons.iconBtnColor.classList.toggle("fi-sr-moon");
+        variable.nav.menu.classList.toggle("light-nav");
+        variable.nav.btnMenu.forEach((btnMenu) => {
+            btnMenu.classList.toggle("light-gradiente");
+        });
+        variable.nav.txtMenu.forEach((btnTxt) => {
+            btnTxt.classList.toggle("light-btnTxt");
+        });  
+        variable.icons.iconBtnTxt.forEach((btnIconTxt) => {
+            btnIconTxt.classList.toggle('light-btnTxt')
+        }); 
+        variable.icons.iconBtnExplorer.classList.toggle('light-btnTxt');
+        variable.nav.btnColor.classList.toggle('light-btnColor');
+        
+        toLight.body.classList.toggle("light2");
+        toLight.socialContainer.classList.toggle("light");
+        toLight.btnRepository.classList.toggle("light-gradiente");
+        toLight.homeh1.classList.toggle("home-color-write")
+        toLight.btnHome.classList.toggle("light-gradiente-text")
 })
 
 
