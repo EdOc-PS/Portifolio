dc9bb636551cced7dbc9b6303aa08ac0c82576e3
Eduardo Octávio <eeuardooctavio@gmail.com>
2024-04-19 23:53:52 -0300

    u-0.6

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,19 +9,31 @@ const variable = {
     icons:{
         iconBtnMenu: document.querySelector('.fi.fi-sr-menu-burger'),
         iconBtnColor: document.querySelector('.fi.fi-sr-brightness'),
-        iconBtnTxt: document.querySelectorAll('.menu-icon i'),
-        iconBtnExplorer: document.querySelector('.home-a-btn i'),
+
     },
     section: document.querySelectorAll('.hidden')
 };
 
 var toLight = {
-    body: document.querySelector("body"),
-    socialContainer: document.querySelector('.about-social-container'),
-    btnRepository: document.querySelector('.discover-repository'),
-    homeh1: document.getElementById('home-h1'),
-    btnHome: document.querySelector('.home-a-btn'),
-
+    global:{
+        body: document.querySelector("body"),
+        iconsColor: document.querySelectorAll('i'),
+        p: document.querySelectorAll('p'),
+        h4: document.querySelectorAll('h4')
+    },
+    several:{
+        socialContainer: document.querySelector('.about-social-container'),
+        btnRepository: document.querySelector('.discover-repository'),
+        homeh1: document.getElementById('home-h1'),
+        btnHome: document.querySelector('.home-a-btn'),
+        aboutDescriptionH1: document.querySelector('.about-mydescreption h1'),
+        aboutDescriptionSpan: document.querySelector('.about-mydescreption span'),
+        socialUl: document.querySelector('.mysocial-ul'),
+        infoTxt: document.querySelector('.about-info-text'),
+        infoMore: document.querySelectorAll('.about-more-text'),
+        iconsInfoMore: document.querySelectorAll('.about-more-text i'),
+    }
+    
 }
 
 //menu se expandir
@@ -42,18 +54,37 @@ variable.nav.btnColor.addEventListener('click', function () {
         variable.nav.txtMenu.forEach((btnTxt) => {
             btnTxt.classList.toggle("light-btnTxt");
         });  
-        variable.icons.iconBtnTxt.forEach((btnIconTxt) => {
-            btnIconTxt.classList.toggle('light-btnTxt')
-        }); 
-        variable.icons.iconBtnExplorer.classList.toggle('light-btnTxt');
         variable.nav.btnColor.classList.toggle('light-btnColor');
+
+        toLight.global.iconsColor.forEach((iconsColor) => {
+            iconsColor.classList.toggle("light-global-color")
+        }); 
+        toLight.global.p.forEach((pColor) => {
+            pColor.classList.toggle("light-global-color");
+        }); 
+        toLight.global.h4.forEach((h4Color) =>{
+            h4Color.classList.toggle("light-infoMore-color");
+        })
+
+        toLight.global.body.classList.toggle("light_2");
+        toLight.several.socialContainer.classList.toggle("light-container");
+        toLight.several.btnRepository.classList.toggle("light-gradiente");
+        toLight.several.homeh1.classList.toggle("home-color-write");
+        toLight.several.btnHome.classList.toggle("light-gradiente-text");
+        toLight.several.aboutDescriptionH1.classList.toggle("home-color-write");
+        toLight.several.aboutDescriptionSpan.classList.toggle("home-color-write");
+        toLight.several.socialUl.classList.toggle("light-gradiente")
+        toLight.several.infoTxt.classList.toggle("light-container")
+
+        toLight.several.infoMore.forEach((infoMore)=> {
+            infoMore.classList.toggle("light-container_2")
+        });
+
+        toLight.several.iconsInfoMore.forEach((infoMore) =>{
+            infoMore.classList.toggle("light-infoMore-color")
+        })
         
-        toLight.body.classList.toggle("light2");
-        toLight.socialContainer.classList.toggle("light");
-        toLight.btnRepository.classList.toggle("light-gradiente");
-        toLight.homeh1.classList.toggle("home-color-write")
-        toLight.btnHome.classList.toggle("light-gradiente-text")
-})
+})      
 
 
 //botão fazendo menu se desexpandir
